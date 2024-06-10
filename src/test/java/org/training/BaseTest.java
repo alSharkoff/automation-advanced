c04cbbbfeb5add162cd7494f0525e8168b91770f
Oleksii Sharkov <oleksii_sharkov@epam.com>
2024-06-10 12:43:40 +0300

    CI/CD Report Portal Integration: - fixed integration by changing agent version

--- src/test/java/org/training/BaseTest.java
+++ src/test/java/org/training/BaseTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Listeners;
 import org.training.configuration.ConfigHelper;
 
 @Listeners({ReportPortalTestNGListener.class})
-//@ExtendWith(ReportPortalExtension.class)
+@ExtendWith(ReportPortalExtension.class)
 public class BaseTest {
     protected static ConfigHelper configHelper = new ConfigHelper();
 }
