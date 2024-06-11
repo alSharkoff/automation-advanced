308382db8810182d9b8dbfc2aa962911071b7eb4
Oleksii Sharkov <oleksii_sharkov@epam.com>
2024-06-11 16:31:38 +0300

    Sonar Test 1

--- src/test/java/org/training/BaseTest.java
+++ src/test/java/org/training/BaseTest.java
@@ -5,6 +5,7 @@ import com.epam.reportportal.testng.ReportPortalTestNGListener;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testng.annotations.Listeners;
 import org.training.configuration.ConfigHelper;
+import org.training.configuration.ConfigHelper;
 
 @Listeners({ReportPortalTestNGListener.class})
 @ExtendWith(ReportPortalExtension.class)
